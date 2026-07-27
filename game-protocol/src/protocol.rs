1d86b64dc062af78d2a535734a82f4e840d2b27d
Burvye <thebuffonion@gmail.com>
2026-07-26 23:35:09 -0400

    fix: make comment in gitignore clearer and pub the consts in protocol
    Signed-off-by: Burvye <thebuffonion@gmail.com>

--- game-protocol/src/protocol.rs
+++ game-protocol/src/protocol.rs
@@ -6,8 +6,8 @@ use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use bevy::prelude::*;
 use serde::{Deserialize, Serialize};
 
-const TIMESTEP: f64 = 1.0 / 64.0;
-const SERVER_ADDR: SocketAddr = SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 0);
+pub const TIMESTEP: f64 = 1.0 / 64.0;
+pub const SERVER_ADDR: SocketAddr = SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 0);
 
 /// Marker used to mark something as a player (no fields)
 /// This struct is bolted onto players as components, and thus must derive Component.
