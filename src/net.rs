271b2435f4f462dad6537ee402f2af1587370f64
Burvye <thebuffonion@gmail.com>
2026-08-09 12:07:10 -0400

    feat: connect function
    Signed-off-by: Burvye <thebuffonion@gmail.com>

--- src/net.rs
+++ src/net.rs
@@ -4,12 +4,15 @@
 
 use std::{
     net::{IpAddr, Ipv4Addr, SocketAddr},
-    time::Duration,
+    time::{Duration, SystemTime, UNIX_EPOCH},
 };
 
 use bevy::prelude::*;
 use game_protocol::protocol;
-use lightyear::prelude::{client::ClientPlugins, *};
+use lightyear::{
+    netcode::Key,
+    prelude::{client::ClientPlugins, *},
+};
 
 const CLIENT_ADDR: SocketAddr = SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 0);
 
@@ -20,5 +23,18 @@ impl Plugin for NetPlugin {
         app.add_plugins(ClientPlugins {
             tick_duration: Duration::from_secs_f64(protocol::TIMESTEP),
         });
+        app.add_systems(Startup, connect);
     }
+}
+
+fn connect(mut cmds: Commands) -> Result {
+    let digest = std::fs::read_to_string("digest.txt")?.trim().to_string();
+    let auth = Authentication::Manual {
+        server_addr: protocol::SERVER_ADDR,
+        // TODO: use a more secure id so hackers dont spoof other peoples' ids
+        client_id: SystemTime::now().duration_since(UNIX_EPOCH)?.as_nanos() as u64,
+        private_key: Key::default(),
+        protocol_id: 0,
+    };
+    Ok(())
 }
