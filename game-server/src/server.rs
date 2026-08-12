9dc87588f912813db9bb1328d01b57c373db9483
Burvye <thebuffonion@gmail.com>
2026-08-12 18:30:37 -0400

    docs: remove logging tags
    Signed-off-by: Burvye <thebuffonion@gmail.com>

--- game-server/src/server.rs
+++ game-server/src/server.rs
@@ -104,7 +104,7 @@ fn server_player_motion(
     mut players: Query<(
         &mut Rotation,
         &mut LinearVelocity,
-        Entity,
+        Entity, // TODO: Remove logging
         &ActionState<protocol::PlayerInputs>,
     )>,
 ) {
@@ -112,6 +112,7 @@ fn server_player_motion(
         .iter_mut()
         .for_each(|(mut rot, mut vel, entity, action)| {
             if action.motion != Vec2::ZERO {
+                // TODO: Remove logging
                 print!("entity: {:?} is moving: {:?}\n", entity, action.motion);
             }
             shared::apply_input(&mut rot, &mut vel, &action.0);
