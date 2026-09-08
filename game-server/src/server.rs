1b46b01f02f55f7c3c59baaa759d96db467b0f5f
Burvye <thebuffonion@gmail.com>
2026-09-08 14:30:48 -0400

    feat: longer disconnection secs and fix
    Signed-off-by: Burvye <thebuffonion@gmail.com>

--- game-server/src/server.rs
+++ game-server/src/server.rs
@@ -80,8 +80,8 @@ fn startup(mut cmds: Commands) -> Result {
     let server = cmds
         .spawn((
             NetcodeServer::new(NetcodeConfig {
-                client_timeout_secs: 30 // how many seconds before disconnection
-                ..default(),
+                client_timeout_secs: 60, // how many seconds before disconnection
+                ..default()
             }),
             LocalAddr(protocol::SERVER_BIND_ADDR),
             WebTransportServerIo {
